49edabb5756e8e64890439d02b633c2c9afcd015
pallab-gain <pallab.gain@gmail.com>
2021-06-06 16:40:53 +0300

    Add ts and initial project code

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "extends": ["plugin:@typescript-eslint/recommended"],
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": 1,
+        "@typescript-eslint/no-inferrable-types": [
+            "warn", {
+                "ignoreParameters": true
+            }
+        ],
+        "@typescript-eslint/no-unused-vars": "warn"
+    },
+};
