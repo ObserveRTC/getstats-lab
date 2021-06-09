beee085b3d2459c32c583dc085daef2b52ecba57
pallab-gain <pallab.gain@gmail.com>
2021-06-09 04:16:07 +0300

    Removed redundant browser options

--- src/browser.capabilities/index.ts
+++ src/browser.capabilities/index.ts
@@ -1,8 +1,26 @@
-import {
-    chromeBypassUserMediaOptions,
-    edgeBypassUserMediaOptions,
-    firefoxBypassUserMediaOptions
-} from '../browser.options'
+const chromeBypassUserMediaOptions = {
+    'goog:chromeOptions': {
+        'args': ['--use-fake-device-for-media-stream', '--use-fake-ui-for-media-stream']
+    }
+}
+
+const firefoxBypassUserMediaOptions = {
+    'moz:firefoxOptions': {
+        prefs: {
+            'media.navigator.streams.fake': true,
+            'media.navigator.permission.disabled': true,
+            'media.autoplay.default': 0,
+            'media.volume_scale': '0.0'
+        }
+    }
+}
+
+const edgeBypassUserMediaOptions = {
+    'ms:edgeOptions': {
+        'args': ['--use-fake-device-for-media-stream', '--use-fake-ui-for-media-stream']
+    }
+}
+
 
 const defaultCapabilities = {
     'os' : 'Windows',
