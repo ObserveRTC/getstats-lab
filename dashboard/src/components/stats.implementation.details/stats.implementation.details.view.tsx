c3503686556d116f338438d8b1527d1a034c3d14
pallab-gain <pallab.gain@gmail.com>
2021-06-12 11:59:21 +0300

    Before JSON viewer

--- dashboard/src/components/stats.implementation.details/stats.implementation.details.view.tsx
+++ dashboard/src/components/stats.implementation.details/stats.implementation.details.view.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {StatsDetails} from "../../redux/root.slice";
+import styles from './stats.implementation.details.module.scss'
 
 export type StatsImplementationDetailsViewProps = {
-    statsList: StatsDetails | undefined
+    statsList: StatsDetails
 }
 const StatsImplementationDetailsView = ({statsList}: StatsImplementationDetailsViewProps): React.ReactElement => {
     return (
-        <div>
+        <div className={styles.container}>
             <>Browser vendor</>
             <>Browser version</>
         </div>
