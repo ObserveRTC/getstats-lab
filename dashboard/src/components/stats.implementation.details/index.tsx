06b975f12d7ea625890aa5a8fdf17910798e4a40
pallab-gain <pallab.gain@gmail.com>
2021-06-13 13:36:21 +0300

    Can render implemented stats

--- dashboard/src/components/stats.implementation.details/index.tsx
+++ dashboard/src/components/stats.implementation.details/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {StatsImplementationDetailsView} from "./stats.implementation.details.view";
 import {
     BrowserDetail,
@@ -8,21 +8,27 @@ import {
 } from "../../redux/root.slice";
 import {useAppDispatch, useAppSelector} from "../../redux/hooks";
 import {BrowserListView} from "../browser.list";
+import styles from './stats.implementation.details.module.scss'
 
 const StatsImplementationDetails = (): React.ReactElement => {
     const dispatch = useAppDispatch();
+    const [currentBrowser, setCurrentBrowser] = useState<BrowserDetail>({
+        browser: '',
+        version: ''
+    })
     const browserList = useAppSelector(selectBrowserList)
-    const statsList = useAppSelector(selectImplementationDetails({browser: 'Chrome', version: '90'}))
+    const statsList = useAppSelector(selectImplementationDetails(currentBrowser))
 
     const onBrowserSelected = ({browser, version}: BrowserDetail) => {
+        setCurrentBrowser({browser, version})
         dispatch(fetchBrowserImplementationDetailsAsync({browser,version}))
     }
 
     return (
-        <>
+        <div className={styles.parentContainer}>
             <BrowserListView onSelected={onBrowserSelected} browserList={browserList}/>
             <StatsImplementationDetailsView statsList={statsList}/>
-        </>
+        </div>
 
     )
 }
--- dashboard/src/components/stats.implementation.details/stats.implementation.details.view.tsx
+++ dashboard/src/components/stats.implementation.details/stats.implementation.details.view.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import {StatsDetails} from "../../redux/root.slice";
 import styles from './stats.implementation.details.module.scss'
+const JSONTreeView = require('react-json-inspector');
+
 
 export type StatsImplementationDetailsViewProps = {
     statsList: StatsDetails
 }
+
+const getCount = (statsList: StatsDetails): number => {
+    return Object.values(statsList ?? {}).reduce((sum, current) => sum + current.length, 0)
+}
+
 const StatsImplementationDetailsView = ({statsList}: StatsImplementationDetailsViewProps): React.ReactElement => {
     return (
         <div className={styles.container}>
-            <>Browser vendor</>
-            <>Browser version</>
+            <div>
+                <div>
+                    Total Stats : { getCount(statsList) }
+                </div>
+                <div>
+                    {statsList && <JSONTreeView data={statsList}/>}
+                </div>
+            </div>
         </div>
     )
 }
